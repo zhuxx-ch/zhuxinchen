39288d4765148699bdbdf8d69197c793757839be
zhuxx-ch <76579906+zhuxx-ch@users.noreply.github.com>
2022-07-03 19:50:54 +0800

    web

---
+++ zhuxinchen-appplication/zhuxinchen-web/src/main/java/com/cnjh/zhuxinchen/web/ZhuxinchenWebApplication.java
@@ -0,0 +1,13 @@
+package com.cnjh.zhuxinchen.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhuxinchenWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhuxinchenWebApplication.class, args);
+    }
+
+}
---
+++ zhuxinchen-appplication/zhuxinchen-web/src/test/java/com/cnjh/zhuxinchen/web/ZhuxinchenWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnjh.zhuxinchen.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhuxinchenWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
