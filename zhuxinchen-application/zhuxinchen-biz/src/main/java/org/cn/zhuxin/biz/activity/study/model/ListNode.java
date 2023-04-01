e6dbc33507fe5487b9a4e7bf7c4fb9ebede20d06
zhuxx <xiaoxingzhu90@163.com>
2023-04-01 17:50:48 +0800

    lombok dependency

--- zhuxinchen-application/zhuxinchen-biz/src/main/java/org/cn/zhuxin/biz/activity/study/model/ListNode.java
+++ zhuxinchen-application/zhuxinchen-biz/src/main/java/org/cn/zhuxin/biz/activity/study/model/ListNode.java
@@ -2,12 +2,22 @@ package org.cn.zhuxin.biz.activity.study.model;
 
 import lombok.Data;
 
+/**
+ * 链表节点
+ */
 @Data
 public class ListNode {
+    /**
+     * 当前节点的值
+     */
     private int val;
+    /**
+     * 下一个节点
+     */
     private ListNode next = null;
 
     public ListNode() {}
+
     public ListNode(int val) {
         this.val = val;
     }
