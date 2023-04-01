f906cba59f4b77497a31d54bc34465500a9eefb8
zhuxx <xiaoxingzhu90@163.com>
2023-04-01 17:49:06 +0800

    lombok dependency

--- zhuxinchen-application/zhuxinchen-biz/src/main/java/org/cn/zhuxin/biz/activity/study/model/ListNode.java
+++ zhuxinchen-application/zhuxinchen-biz/src/main/java/org/cn/zhuxin/biz/activity/study/model/ListNode.java
@@ -1,5 +1,8 @@
 package org.cn.zhuxin.biz.activity.study.model;
 
+import lombok.Data;
+
+@Data
 public class ListNode {
     private int val;
     private ListNode next = null;
@@ -11,22 +14,6 @@ public class ListNode {
 
     public ListNode(int val, ListNode next) {
         this.val = val;
-        this.next = next;
-    }
-
-    public int getVal() {
-        return val;
-    }
-
-    public void setVal(int val) {
-        this.val = val;
-    }
-
-    public ListNode getNext() {
-        return next;
-    }
-
-    public void setNext(ListNode next) {
         this.next = next;
     }
 }
